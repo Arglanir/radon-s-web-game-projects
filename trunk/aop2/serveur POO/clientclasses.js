cb198b8165bf4b01e1bae049f849911a8464c713
arglanir <arglanir@8e3c144e-ebad-11dd-97d9-e7a3ff7b18fb>
2009-02-12 20:50:27 +0000

    Traitement du jeu terminé ! Client OK (juste réadapté) Les joueurs peuvent s'ajouter. Et plein d'autres trucs...

--- trunk/aop2/serveur POO/clientclasses.js
+++ trunk/aop2/serveur POO/clientclasses.js
@@ -1,4 +1,13 @@
-var serveur = "jeu.php";
+var serveur = "serveur.php";
+
+//arguments de l'appel de la page
+var bouh = location.search.substring(1,location.search.length).split(unescape("%26"));//on enlève le ? et on sépare avec les &
+var tableauArguments = new Array();
+tableauArguments["offline"] = 0;
+for (i=0;i<bouh.length;i++){
+   var temp = bouh[i].split("=");
+    tableauArguments[temp[0]]=unescape(temp[1]);
+}
 
 function createXHR() 
 {
@@ -36,16 +45,17 @@ function patiente(millis) {
 }
 
 //fonction de communication avec le serveur
-function communique(tableauArgs,callback){
+function communiqueGET(tableauArgs,callback){
 	var xhr = createXHR();
-	var chaineDAppel = serveur+"?";
+	var chaineDAppel = "";
 	if (typeof(tableauArgs)=='string'){
-		chaineDAppel = tableauArgs+"?nocache=" + Math.random();
+		chaineDAppel = tableauArgs+(tableauArgs.indexOf("?")>=0?"&":"?")+"nocache=" + Math.random();
 	}
 	else {//tableau associatif
-	for(var clef in tableauArgs)
-		chaineDAppel+=clef+"="+tableauArgs[clef]+"&";
-	chaineDAppel += "nocache=" + Math.random();
+		chaineDAppel+=serveur+"?";
+		for(var clef in tableauArgs)
+			chaineDAppel+=clef+"="+tableauArgs[clef]+"&";
+		chaineDAppel += "nocache=" + Math.random();
 	}
 	xhr.onreadystatechange  = function(){ 
 		if(xhr.readyState  == 4){
@@ -59,7 +69,8 @@ function communique(tableauArgs,callback){
 				}
 				else {
 					document.getElementById("comm").innerHTML = "";
-					callback(xhr.responseXML);
+					if (callback)
+						callback(xhr.responseXML);
 				}
 			} else {
 				document.getElementById("comm").innerHTML = "La communication "+chaineDAppel+" n'a pas abouti.";
@@ -73,6 +84,60 @@ function communique(tableauArgs,callback){
 	
 }
 
+function communiquePOST(tableauArgsGET,tableauArgsPOST,callback){
+	var xhr = createXHR();
+	var chaineDAppel = serveur+"?";
+	if (typeof(tableauArgsGET)=='string'){
+		chaineDAppel = tableauArgsGET+(tableauArgsGET.indexOf("?")>=0?"&":"?")+"nocache=" + Math.random();
+	}
+	else {//tableau associatif
+	for(var clef in tableauArgsGET)
+		chaineDAppel+=clef+"="+tableauArgsGET[clef]+"&";
+	chaineDAppel += "nocache=" + Math.random();
+	}
+	chaineEnvoyee = "";
+	for(var clef in tableauArgsPOST)
+		chaineEnvoyee+=clef+"="+encodeURIComponent(tableauArgsPOST[clef])+"&";
+	chaineEnvoyee = chaineEnvoyee.substr(0,chaineEnvoyee.length-1);
+	xhr.onreadystatechange  = function(){ 
+		if(xhr.readyState  == 4){
+			if(xhr.status  == 200) {
+				leXML = xhr.responseXML;
+				yaErreur = leXML.getElementsByTagName("erreur").length;
+				if (yaErreur){
+					document.getElementById("comm").innerHTML = 
+						"Erreur : "+leXML.getElementsByTagName("erreur").item(0).getAttribute('raison')+
+						" lors de "+leXML.getElementsByTagName("erreur").item(0).getAttribute('origine')+".";
+				}
+				else {
+					document.getElementById("comm").innerHTML = "";
+					if (callback)
+						callback(xhr.responseXML,xhr.responseText);
+				}
+			} else {
+				document.getElementById("comm").innerHTML = "La communication "+chaineDAppel+" n'a pas abouti.";
+			}
+         }
+    }; 
+	document.getElementById("comm").innerHTML = "Attente du serveur...";
+	
+	xhr.open("POST", chaineDAppel, true); 
+	xhr.send(chaineEnvoyee);
+	
+}
+
+function envoieFormulaire(tableauArgsGET,formulaire,callback){
+	var liste = formulaire.elements;
+	var tableauPOST = new Array();
+	for (var clef in liste){
+		var lInput = liste[clef];
+		if (lInput.value)
+			tableauPOST[lInput.name] = (lInput.value?lInput.value:lInput.checked);
+		if (lInput.checked)
+			tableauPOST[lInput.name] = "1";
+	}
+	communiquePOST(tableauArgsGET,tableauPOST,callback);
+}
 
 function Partie(){
 	
--- trunk/aop2/serveur POO/creajeu.php
+++ trunk/aop2/serveur POO/creajeu.php
@@ -4,9 +4,12 @@ Créateur de jeu appelé par un client
 *************************/
 include_once ("newjeux.php");
 
-
 $jeu = new CreaJeu();
+if (array_key_exists("enxml",$_GET))
+	if ($_GET["enxml"]=="1"){
+		$jeu->affichageInfosXML();
+		die();
+	}
 $jeu->affichageLiensPartie();
-
 //c'est bateau...
 ?>
